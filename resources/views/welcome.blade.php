b8c39c7584214f356b9a047c1c665f91414698ef
risdian <dxlift@gmail.com>
2020-09-08 14:19:41 +0800

    edit profile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Install la hanat
                 </div>
 
                 <div class="links">
